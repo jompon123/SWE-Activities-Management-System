e682231fad903b5335ec79ed86722b41624da3fd
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2018-11-12 10:51:47 +0700

    edit frontend pagelogin

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,10 +1,16 @@
 <html>
   <head>
+
     <link rel="stylesheet" href="{{asset('assets/css/login/login.css')}}">
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+<style>
+body{
+    background-color: #477279;
+}
+</style>
   </head>
 <body id="LoginForm">
 <div class="container">
@@ -12,15 +18,15 @@
 <div class="login-form">
 <div class="main-div">
     <div class="panel">
-   <h2>Admin Login</h2>
-   <p>Please enter your email and password</p>
+   <h2>Login</h2>
+   <p>Please enter your username and password</p>
    </div>
     <form id="Login">
 
         <div class="form-group">
 
 
-            <input type="email" class="form-control" id="inputEmail" placeholder="Email Address">
+            <input type="email" class="form-control" id="inputUsername" placeholder="Username">
 
         </div>
 
@@ -36,7 +42,6 @@
 
     </form>
     </div>
-<p class="botto-text"> Designed by Sunil Rajput</p>
 </div></div></div>
 
 
