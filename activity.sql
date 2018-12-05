ec6cffc9eb8276b74f417e8233e6d137a76edb44
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2018-12-05 18:06:38 +0700

    update db-activity

--- activity.sql
+++ activity.sql
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS `activity`;
 CREATE TABLE `activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `activity_name` char(255) COLLATE utf8_unicode_ci NOT NULL,
-  `descripton` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
   `teacher` text COLLATE utf8_unicode_ci NOT NULL,
   `day_start` datetime NOT NULL,
   `day_end` datetime NOT NULL,
@@ -22,11 +22,19 @@ CREATE TABLE `activity` (
   `term_year` int(11) NOT NULL,
   `sector` int(11) NOT NULL,
   `location` text COLLATE utf8_unicode_ci NOT NULL,
-  `image` text COLLATE utf8_unicode_ci NOT NULL,
-  `student` text COLLATE utf8_unicode_ci NOT NULL,
+  `image` text COLLATE utf8_unicode_ci,
+  `student` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `created_by` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `activity` (`id`, `activity_name`, `description`, `teacher`, `day_start`, `day_end`, `time_start`, `time_end`, `term_year`, `sector`, `location`, `image`, `student`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+(1,	'swe222',	'',	'',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	0,	0,	'',	'',	'',	'2018-12-05 09:54:09',	'',	'2018-12-05 09:54:09',	''),
+(2,	'ติวน้อง',	'นะๆ',	'2558',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	2,	2561,	'355',	NULL,	NULL,	'2018-12-05 10:07:34',	'',	'2018-12-05 10:07:34',	''),
+(3,	'swe',	'ค่ายศึกษางาน',	'3',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	'0000-00-00 00:00:00',	2,	2561,	'ตึกนวัตกรรม',	NULL,	NULL,	'2018-12-05 10:15:06',	'',	'2018-12-05 10:15:06',	'');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -44,4 +52,4 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `created_by`, `
 (1,	'58145236',	'58145236',	'2018-11-21 21:14:09',	'sudarat',	'2018-11-21 21:14:09',	'sudarat'),
 (4,	'setset',	'set',	'2018-11-21 15:37:42',	'',	'2018-11-21 15:37:42',	NULL);
 
--- 2018-12-05 09:00:46
+-- 2018-12-05 10:16:24
