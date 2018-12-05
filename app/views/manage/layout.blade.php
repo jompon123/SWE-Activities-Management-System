f1c8eff4783382db4aa3eac23a1d18fbea959037
namfonnnnn <sp.sudaratphiwon@hotmail.com>
2018-12-06 03:29:33 +0700

    เพิ่ม cdn ไว้ใส่ไฟล์ที่มาจากข้างนอก

--- app/views/manage/layout.blade.php
+++ app/views/manage/layout.blade.php
@@ -6,7 +6,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    
+        @yield('cdn')
     </head>
     <body>
    
